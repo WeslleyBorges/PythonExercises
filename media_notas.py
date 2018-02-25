3d2db112e1ce97b4a8a05a87a1ce097dd3c3ac3c
Weslley Borges <weslleynfs@gmail.com>
2018-02-25 01:26:56 -0300

    Commit

---
+++ media_notas.py
@@ -0,0 +1,12 @@
+#Calcular a média de 5 notas
+
+notas = [6.5, 7.5, 3.4, 8.9, 5.4]
+
+soma_notas = 0;
+
+for x in range(1, len(notas)):
+    soma_notas += notas[x]
+
+media = soma_notas/len(notas)
+
+print("A media das notas é igual a {0}" .format(media))
