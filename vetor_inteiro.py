2eb269d0a0a90125cf829aa1b0122ad41e8faac4
Weslley Borges <weslleynfs@gmail.com>
2018-02-25 01:47:52 -0300

    Inserção de mais uma forma de exibição do vetor.

--- vetor_inteiro.py
+++ vetor_inteiro.py
@@ -3,7 +3,13 @@ vetor_inteiro = [];
 for x in range(0,5):
     vetor_inteiro.append(int(input('Insira um número na posição ' + str(x) + ' do vetor: ')))
 
-print('\n')
+print()
 
+print('Posição por posição: ')
 for x in range(0,len(vetor_inteiro)):
     print('Posição {0}: {1}' .format(x, vetor_inteiro[x]))
+
+print()
+
+print('De cabo a rabo: ')
+print(vetor_inteiro)
